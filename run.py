f8fa00dbabe1cfe75385c92a751b0d0a6b623152
LXY <13529830360@163.com>
2021-08-23 08:50:57 +0800

    测试push

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from base.config import *
 if __name__ == "__main__":
     pytest.main([SCRIPT_PATH, '--alluredir', TEMP_PATH])
     os.system('allure generate '+TEMP_PATH+' -o '+REPORT_PATH+' --clean')
+    print("test")
     if DING == 'yes':
         """ 推送 dingding 消息 """
         print(DING_MSG["markdown"]["text"])
